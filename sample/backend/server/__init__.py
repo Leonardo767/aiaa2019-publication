3de33146c97319af9d492cdbe7c491da322adebf
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2019-03-14 03:05:41 -0500

    Added connection to MongoDB

--- sample/backend/server/__init__.py
+++ sample/backend/server/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask.ext.pymongo import pymongo
+
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'b2e7a328a524163dfb96a7a8a3b6c15a'
+app.config['MONGO_DBNAME'] = 'dummy'
+app.config['MONGO_URI'] = 'dummy'
 
 from server import routes
