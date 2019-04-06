ccef0017d5924d652caae330de9246f50002c8cb
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2019-04-06 18:09:50 -0500

    Integrated MySQL connection
    NOTE: YAML file is not contained for security

--- sample/server/__init__.py
+++ sample/server/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+from flask_mysqldb import MySQL
+import yaml
+
+# NOTE: yaml file is pulled outside of app directory to obscure sensitive information
+# to use your own database, please modify open_resources() path to your own yaml file containing the keys called here
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = 'b2e7a328a524163dfb96a7a8a3b6c15a'
-app.config['MONGO_DBNAME'] = 'dummy'
-app.config['MONGO_URI'] = 'dummy'
+with app.open_resource('../../../YAML/aiaa_2019_sensitive.yaml') as f:
+    sensitive_info = yaml.safe_load(f)
+app.config['MYSQL_HOST'] = sensitive_info['mysql_host']
+app.config['MYSQL_USER'] = sensitive_info['mysql_user']
+app.config['MYSQL_PASSWORD'] = sensitive_info['mysql_password']
+app.config['MYSQL_DB'] = sensitive_info['mysql_db']
+
+app.config['SECRET KEY'] = sensitive_info['secret_key']
+
+mysql = MySQL(app)
 
 from server import routes
--- sample/server/routes.py
+++ sample/server/routes.py
@@ -1,8 +1,27 @@
-from flask import render_template, url_for, flash, redirect
-from server import app
-from server.createPath import Geo
+from flask import render_template, url_for, flash, redirect, request
+from flask_mysqldb import MySQL
+from server import app, mysql
+from server.lib.objects import Geo
 
 
 @app.route('/', methods=['GET', 'POST'])
 def input():
+    if request.method == 'POST':
+        # fetch form data
+        GeoData = request.form
+        name = GeoData['GeoName']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Geo(GeoName) VALUES(%s)", [name])
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('database'))
     return render_template('input.html')
+
+
+@app.route('/database')
+def database():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM Geo")
+    if resultValue > 0:
+        GeoData = cur.fetchall()
+        return render_template('database.html', GeoData=GeoData)
