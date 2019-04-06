19e4638df71fa34612023e7836a48645b74de091
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2019-04-06 18:43:14 -0500

    Added Workflow Structure

--- sample/server/__init__.py
+++ sample/server/__init__.py
@@ -3,10 +3,13 @@ from flask_mysqldb import MySQL
 import yaml
 
 # NOTE: yaml file is pulled outside of app directory to obscure sensitive information
-# to use your own database, please modify open_resources() path to your own yaml file containing the keys called here
+# to use your own database, please modify app.open_resources() path to your own yaml file containing the keys called here
+
+# NOTE: specifically for this __init__.py file, save file without auto-formatting
+# on VSCode, the keyboard command to save without auto-formatting is: "Ctrl + K", and then "Ctrl + Shift + S"
 
 app = Flask(__name__)
-with app.open_resource('../../../YAML/aiaa_2019_sensitive.yaml') as f:
+with app.open_resource('../../../YAML/aiaa_2019_sensitive.yaml') as f:  # path is unique to local machine
     sensitive_info = yaml.safe_load(f)
 app.config['MYSQL_HOST'] = sensitive_info['mysql_host']
 app.config['MYSQL_USER'] = sensitive_info['mysql_user']
--- sample/server/routes.py
+++ sample/server/routes.py
@@ -5,7 +5,8 @@ from server.lib.objects import Geo
 
 
 @app.route('/', methods=['GET', 'POST'])
-def input():
+@app.route('/input_environment', methods=['GET', 'POST'])
+def input_environment():
     if request.method == 'POST':
         # fetch form data
         GeoData = request.form
@@ -14,14 +15,36 @@ def input():
         cur.execute("INSERT INTO Geo(GeoName) VALUES(%s)", [name])
         mysql.connection.commit()
         cur.close()
-        return redirect(url_for('database'))
-    return render_template('input.html')
+    return render_template('input_environment.html')
 
 
-@app.route('/database')
-def database():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM Geo")
-    if resultValue > 0:
-        GeoData = cur.fetchall()
-        return render_template('database.html', GeoData=GeoData)
+@app.route('/input_sim', methods=['GET', 'POST'])
+def input_sim():
+    if request.method == 'POST':
+        # fetch form data
+        pass
+    return render_template('input_sim.html')
+
+
+@app.route('/execute', methods=['GET', 'POST'])
+def execute():
+    if request.method == 'POST':
+        # fetch form data
+        pass
+    return render_template('execute.html')
+
+
+@app.route('/progress', methods=['GET', 'POST'])
+def progress():
+    if request.method == 'POST':
+        # fetch form data
+        pass
+    return render_template('progress.html')
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'POST':
+        # fetch form data
+        pass
+    return render_template('results.html')
