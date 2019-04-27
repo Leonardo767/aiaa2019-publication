59876ffe5ec10d59a2599c070540007f43c248c8
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2019-04-27 11:52:00 -0500

    delta computation in PyTorch

--- sample/server/src/optimization/torch_trial.py
+++ sample/server/src/optimization/torch_trial.py
@@ -21,61 +21,117 @@ def find_theta(X_n, X_o, X_o_point, d_components):
     :param X_o_point: tensor [x_s, y_s, t_s], represents intersection loc for which we must find theta
     :return: theta vector, same length as node vector, except a vector of [theta] scalars rather than [x, y, t] vectors
     """
-    # using cos(theta) = dot(u,v)/(norm(u)*norm(v))...
+    # print('\n\n\n\n\n\n')
+    # print('\n\nd_comp size:', d_components.size())
+    # print('d_comp:', d_components)
+    # using cos(theta) = dot(u,v)/(norm(u)*norm(v)):
+    # ---------------------------------------------------------
     # consistent direction to introduce asymmetry
     sim_vector = (X_o[-1, :] - X_o[0, :])
-    print(sim_vector)
     sim_vector = sim_vector.repeat(d_components.size()[0], 1)
-    print(sim_vector)
-    # print(d_components)
-    # print(sim_vector.size())
-    sim_mag = torch.norm(sim_vector)
+    # print('\n\nsim vect size:', sim_vector.size())
+    # print('sim vect:', sim_vector)
 
-    mag_product = torch.norm(d_components, dim=1) * sim_mag
-    cos_theta_s = torch.dot(d_components, sim_vector) / mag_product
+    # compute dot product
+    dot_prod = torch.bmm(d_components.view(
+        20, 1, 3), sim_vector.view(20, 3, 1))
+    dot_prod = dot_prod.view(-1, 1)
+    # print('\n\ndot size:', dot_prod.size())
+    # print('dot:', dot_prod)
+
+    # compute mag denom
+    sim_mag = torch.norm(sim_vector)
+    mag_denom = torch.norm(d_components, dim=1) * sim_mag
+    mag_denom = mag_denom.view(-1, 1)
+    # print('\n\nmag size:', mag_denom.size())
+    # print('mag:', mag_denom)
+
+    # compute theta
+    cos_theta_s = dot_prod / mag_denom
     theta = torch.acos(cos_theta_s)
-    print(theta)
+    # print('\n\ntheta size:', theta.size())
+    # print('theta:', np.degrees(theta))
     return theta
 
 
-def find_delta_d_s(d_vect, beta_params, sigma_params):
-
-    delta_d_s = 0
+def find_delta_d_s(d_s, beta_params, sigma_params, mu_params):
+    beta = beta_params[0]
+    sigma = sigma_params[0]
+    mu = mu_params[0]
+    d_mag = torch.norm(d_s, dim=1)
+    d_mag = d_mag.view(-1, 1)
+    scaling_factor = 1/(2*np.pi*sigma**2)**0.5
+    exp_factor = -beta*(d_mag - mu)**2/(2*sigma**2)
+    delta_d_s = scaling_factor*torch.exp(exp_factor)
+    max_val = torch.max(delta_d_s)
+    delta_d_s /= max_val
+    # print('s:', delta_d_s)
     return delta_d_s
 
 
-def find_delta_d_e(d_vect, beta_params, sigma_params):
-
-    delta_d_e = 0
+def find_delta_d_e(d_e, beta_params, sigma_params, mu_params):
+    beta = beta_params[1]
+    sigma = sigma_params[1]
+    mu = mu_params[0]
+    d_mag = torch.norm(d_e, dim=1)
+    d_mag = d_mag.view(-1, 1)
+    scaling_factor = 1/(2*np.pi*sigma**2)**0.5
+    exp_factor = -beta*(d_mag - mu)**2/(2*sigma**2)
+    delta_d_e = scaling_factor*torch.exp(exp_factor)
+    max_val = torch.max(delta_d_e)
+    delta_d_e /= max_val
+    # print('e:', delta_d_e)
     return delta_d_e
 
 
-def find_delta_theta_s(X_n, X_o, d_s, beta_params, sigma_params):
+def find_delta_theta_s(X_n, X_o, d_s, beta_params, sigma_params, mu_params):
     theta = find_theta(X_n, X_o, X_o[0, :], d_s)
-    delta_theta_s = 0
+    beta = beta_params[2]
+    sigma = sigma_params[2]
+    mu = mu_params[1]
+    scaling_factor = 1/(2*np.pi*sigma**2)**0.5
+    exp_factor = -beta*(theta - mu)**2/(2*sigma**2)
+    delta_theta_s = scaling_factor*torch.exp(exp_factor)
+    max_val = torch.max(delta_theta_s)
+    delta_theta_s /= max_val
+    # print('s:', delta_theta_s)
     return delta_theta_s
 
 
-def find_delta_theta_e(X_n, X_o, d_e, beta_params, sigma_params):
+def find_delta_theta_e(X_n, X_o, d_e, beta_params, sigma_params, mu_params):
     theta = find_theta(X_n, X_o, X_o[-1, :], d_e)
-    delta_theta_e = 0
+    beta = beta_params[3]
+    sigma = sigma_params[3]
+    mu = mu_params[1]
+    scaling_factor = 1/(2*np.pi*sigma**2)**0.5
+    exp_factor = -beta*(theta - mu)**2/(2*sigma**2)
+    delta_theta_e = scaling_factor*torch.exp(exp_factor)
+    max_val = torch.max(delta_theta_e)
+    delta_theta_e /= max_val
+    # print('e:', delta_theta_e)
     return delta_theta_e
 
 
-def find_delta_n(X_n, X_o, beta_params, sigma_params):
+def find_delta_j(X_n, X_o, beta_params, sigma_params, mu_params):
+    j = torch.Tensor([[i for i in range(X_n.size()[0])]])
+    beta = beta_params[4]
+    sigma = sigma_params[4]
+    mu = mu_params[2]
+    scaling_factor = 1/(2*np.pi*sigma**2)**0.5
+    exp_factor = -beta*(j - mu)**2/(2*sigma**2)
+    delta_j = scaling_factor*torch.exp(exp_factor)
+    print(delta_j)
+    return delta_j
 
-    delta_n = 0
-    return delta_n
 
-
-def compute_delta_vector(X_n, X_o, d_s, d_e, beta_params, sigma_params):
-    delta_d_s = find_delta_d_s(d_s, beta_params, sigma_params)
-    delta_d_e = find_delta_d_e(d_e, beta_params, sigma_params)
+def compute_delta_vector(X_n, X_o, d_s, d_e, beta_params, sigma_params, mu_params):
+    delta_d_s = find_delta_d_s(d_s, beta_params, sigma_params, mu_params)
+    delta_d_e = find_delta_d_e(d_e, beta_params, sigma_params, mu_params)
     delta_theta_s = find_delta_theta_s(
-        X_n, X_o, d_s, beta_params, sigma_params)
+        X_n, X_o, d_s, beta_params, sigma_params, mu_params)
     delta_theta_e = find_delta_theta_e(
-        X_n, X_o, d_e, beta_params, sigma_params)
-    delta_n = find_delta_n(X_n, X_o, beta_params, sigma_params)
+        X_n, X_o, d_e, beta_params, sigma_params, mu_params)
+    delta_j = find_delta_j(X_n, X_o, beta_params, sigma_params, mu_params)
     delta = 0
     return delta
 
@@ -100,7 +156,7 @@ nodes = np.asarray([
     [2.7,	1.3,	10.12],
     [2.8,	1.2,	10.25],
     [2.9,	1.1,	10.38],
-    [3,	1,	10.5]])
+    [3,	    1,	10.5]])
 
 objects = np.asarray([
     [1.81,	2.14,	8.95],
@@ -122,10 +178,15 @@ d_s, d_e = find_distance(X_n, X_o)
 # print(d_e)
 
 torch.manual_seed(0)
-beta_params = torch.randn(5)
-sigma_params = torch.randn(5)
-# print('\nPARAMS:')
-# print(beta_params)
-# print(sigma_params)
+beta_params = torch.abs(torch.randn(5))     # only positive beta allowed
+sigma_params = torch.abs(torch.randn(5))    # only positive sigma allowed
+sigma_params[4] *= X_n.size()[0]            # sigma_j must be scaled to j size
+mu_params = torch.randn(3)
+print('\nPARAMS:')
+print(beta_params)
+print(sigma_params)
+print(mu_params)
 
-delta = compute_delta_vector(X_n, X_o, d_s, d_e, beta_params, sigma_params)
+delta = compute_delta_vector(
+    X_n, X_o, d_s, d_e, beta_params, sigma_params, mu_params)
+# print(delta)
